569e260da0fdf4f80e16055d37de4536847e1bab
Kush Pandya <kush.pandya@aimcrafters.com>
2019-04-17 19:10:18 +0530

    Added reset passowrd activity...

--- app/src/main/java/com/example/chatapplication/Activities/LoginActivity.java
+++ app/src/main/java/com/example/chatapplication/Activities/LoginActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.chatapplication.R;
@@ -15,7 +16,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class LoginActivity extends AppCompatActivity {
@@ -23,7 +23,7 @@ public class LoginActivity extends AppCompatActivity {
     MaterialEditText email, password;
     Button btnLogin;
     FirebaseAuth auth;
-    DatabaseReference reference;
+    TextView forgot_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +39,14 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.pasword);
         btnLogin = findViewById(R.id.buttonLogin);
+        forgot_password = findViewById(R.id.forgot_password);
+
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
